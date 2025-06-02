329c9bf78df5ef6baba4d146ae71f22db50a3d85
Arnaldo Ramirez <arnaldo_45rv@hotmail.com>
2025-06-02 13:40:34 -0500

    cambio de ruta npm

--- src/extension.ts
+++ src/extension.ts
@@ -179,7 +179,7 @@ function createWebview(context: vscode.ExtensionContext, viewId: string, title: 
                 logChannel.appendLine('Zowe CLI Pathh: ' + zowePath);
                 if (!zowePath) return;
                 // Ejecutar el comando Zowe CLI
-                exec(zoweCommand, (error, stdout, stderr) => {
+                exec(zowePath + " " + zoweCommand, (error, stdout, stderr) => {
                     if (handleZoweCommandError(panel, error, stderr, message)) {
                         return;
                     }
