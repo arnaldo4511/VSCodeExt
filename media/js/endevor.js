ecc6f8badb19a31c24477235c7e98a813293fa00
Arnaldo Ramirez <arnaldo_45rv@hotmail.com>
2025-07-20 19:10:20 -0500

    prueba de boton

--- media/js/endevor.js
+++ media/js/endevor.js
@@ -307,9 +307,7 @@ window.addEventListener('message', (event) => {
                     }
 
                     // Construir el texto para mostrar en el <pre>
-                    resultText += <button id="btn-${message.index}" class="btn btn-primary" onclick="vscode.postMessage({ command: 'logMessage', text: 'click: ' + resultText })">Cclick</button> +
-                        '<span id="spanElement-' + message.index + '">' +
-                        '<b>' + elmName + '</b></span>' + blank +
+                    resultText += '<button>Cclick</button>' +
                         formatElmName + blank +
                         formatTypeName + blank +
                         formatEnvName + blank +
